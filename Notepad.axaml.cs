1d1ad023d6cc052b7477160ea7b55c05cfffaf97
Sebastian Veggerby Rasmussen <Sebastian.veggerby@gmail.com>
2023-08-10 11:36:59 +0200

    dawdw

---
+++ Notepad.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GUIH2;
+
+public partial class Notepad : UserControl
+{
+    public Notepad()
+    {
+        InitializeComponent();
+    }
+}
